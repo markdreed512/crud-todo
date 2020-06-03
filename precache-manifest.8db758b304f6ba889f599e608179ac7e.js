49559d4a70d5eb294d05513126588cb7b5409471
Mark Reed <markdreed512@gmail.com>
2020-06-03 11:14:03 -0500

    Updates

---
+++ precache-manifest.8db758b304f6ba889f599e608179ac7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49666e8793f7b4c0d15b620e7b0167ab",
+    "url": "/crud-todo/index.html"
+  },
+  {
+    "revision": "4b7c047704a7d0fcbe44",
+    "url": "/crud-todo/static/css/main.15fb8b98.chunk.css"
+  },
+  {
+    "revision": "c0ed71cd9cd212d02d4c",
+    "url": "/crud-todo/static/js/2.63f26298.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crud-todo/static/js/2.63f26298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b7c047704a7d0fcbe44",
+    "url": "/crud-todo/static/js/main.7fd95d14.chunk.js"
+  },
+  {
+    "revision": "01ab4cec12bc50c18fd5",
+    "url": "/crud-todo/static/js/runtime-main.36b2b7b1.js"
+  }
+]);
